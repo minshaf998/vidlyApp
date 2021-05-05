0a038c57c4f3a8f90b43791117e40632d34f3bd0
minshaf998 <minshaf998@gmail.com>
2021-05-05 09:20:06 +0530

    like component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import reactDom from "react-dom";
 import { getMovies } from "./../services/fakeMovieService";
+import Like from "./common/like";
 
 class Movies extends Component {
   state = { movies: getMovies() };
@@ -8,6 +9,18 @@ class Movies extends Component {
     // console.log(movie);
     // console.log(this.state.movies[0]._id);
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLike = (c) => {
+    const movies = this.state.movies.map((movie) => {
+      if (movie.liked !== true && movie.liked !== false) movie.liked = false;
+      if (movie._id === c._id) {
+        movie.liked = !movie.liked;
+        return movie;
+      }
+      return movie;
+    });
     this.setState({ movies });
   };
 
@@ -35,6 +48,9 @@ class Movies extends Component {
                   <td>{c.genre.name}</td>
                   <td>{c.numberInStock}</td>
                   <td>{c.dailyRentalRate}</td>
+                  <td>
+                    <Like liked={c.liked} onClick={() => this.handleLike(c)} />
+                  </td>
                   <td>
                     <button
                       onClick={() => this.handleClick(c)}
